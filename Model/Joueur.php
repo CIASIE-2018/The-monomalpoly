11d779bc00041f81d6d68a90d112a1a9df233bd6
ThibaultWIRTZ <wirtzthibault@gmail.com>
2018-09-26 15:13:54 +0200

    Initialisation des classes du modèle

--- Model/Joueur.php
+++ Model/Joueur.php
@@ -3,10 +3,35 @@ class Joueur{
 	private $id;
 	private $nom;
 	private $couleur;
-	private $nbDisque = 0;
+	private $position;
+	private $nbDisque;
+	private $listeServeur;
+
+	public Joueur($id,$nom,$couleur){
+		$this->position = 0;
+		$this->nbDisque = 0;
+		$this->listeServeur = [];
+
+		$this->id = $id;
+		$this->nom = $nom;
+		$this->couleur = $couleur;
+		
+	}
 
 	public move(){
 
+	}
+
+	public selectNewCard(){
+
+	}
+
+	public setId($id){
+		$this->id = $id;
+	}
+
+	public getNom(){
+		return $this->nom;
 	}
 }
 ?>
--- Model/Partie.php
+++ Model/Partie.php
@@ -1,14 +1,34 @@
 <?php
+include('Case.php');
+
 class Partie{
-	private $listeJoueur[];
-	private $pateau[];
+	private $listeCouleur = ['red','yellow','orange','blue','green','grey'];
+	private $listeNomCase = [];
+	private $listeJoueur;
+	private $plateau;
+	private $timer;
 
 	public Partie(){
+		$this->listeJoueur = [];
+		$this->timer = new Timer();
 
+		/* Initialisation du plateau (liste de cases)*/
+		for(int $i=0;$i<$listeNomCase.length();$i++){
+			/* Systeme pour l'affichage en x/y */
+
+			$this->plateau.add(new Case($listeNomCase[$i],$x,$y));
+		}
 	}
 
-	public selectNewCard(){
+	public joueurJoin(){
+		//On créé un nouveau joueur
+		$listeJoueur.add(new Joueur($_SESSION['id'],$_SESSION['nom'],$listeCouleur[$listeJoueur.length()]));
+	}
 
+	public checkNbJoueur(){
+		if($listeJoueur.length()<2){
+			restartTimer();
+		}
 	}
 }
 ?>
